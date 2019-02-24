b68b6f7c4d71ba2c5e4c074113b595140ee6b9cb
Frank Stokes <frank@wearereasonablepeople.com>
2019-02-24 09:28:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const connect = handler => (ctx, next) => {
+  if (ctx.isConnection) {
+    return handler(ctx, next);
+  }
+  return next();
+};
+
+const message = (handlerCheck, handler) => (ctx, next) => {
+  if (typeof handlerCheck !== 'function') {
+    throw new TypeError('Hexnut Handle Middleware: handlerCheck must be a function');
+  }
+  if (ctx.isMessage && handlerCheck(ctx.message)) {
+    return handler(ctx, next);
+  }
+  return next();
+};
+
+module.exports = {
+  connect,
+  message
+};
